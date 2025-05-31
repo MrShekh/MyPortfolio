1ac3baa9393f4bfa740a7f77ab01942947e7573c
Shekh Asif <shekhasifofficial2022@gmail.com>
2025-06-01 01:12:39 +0530

    portfolio created 1-06-2025

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I’m Shekh Asif, a Computer Science Engineering student at Rai University, Ahmedabad.
+I specialize in full-stack development using React.js, Flutter, FastAPI, and MongoDB.
+My interests lie in combining AI and software to build real-world, scalable solutions.
+I’m a fast learner, team player, and passionate about delivering high-quality digital products.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
